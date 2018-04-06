822c3c808d610d6b6592d05ec26f72e24422af67
Jeremy Vollant <jeremy.vollant@altran.com>
2018-04-06 16:30:10 +0200

    Fetching Data Playground

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -1,4 +1,7 @@
-const { MongoClient, ObjectID } = require('mongodb');
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     if (err) {
@@ -7,5 +10,37 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log('Connected to MongoDB server');
     const db = client.db('TodoApp');
 
-    client.close();
+    // db
+    //     .collection('Todos') // collection
+    //     // .find({ completed: false }) // condition
+    //     .find({ _id: new ObjectID('5ac6413f0c3110ebaa1065d0') }) // condition
+    //     .toArray()
+    //     .then(docs => {
+    //         console.log('Todos');
+    //         console.log(JSON.stringify(docs, undefined, 2));
+    //     })
+    //     .catch(err => console.log('Unable to fetch todos', err));
+
+    // db
+    //     .collection('Todos') // collection
+    //     .find()
+    //     .count()
+    //     .then(count => {
+    //         console.log(`Todos count: ${count}`);
+    //     })
+    //     .catch(err => console.log('Unable to fetch todos', err));
+
+    db
+        .collection('Users') // collection
+        .find({
+            name: "Mike"
+        }) // condition
+        .toArray()
+        .then(docs => {
+            console.log('Users');
+            console.log(JSON.stringify(docs, undefined, 2));
+        })
+        .catch(err => console.log('Unable to fetch users', err));
+
+    // client.close();
 });
